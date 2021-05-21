f91807b304d32053b0caa77bc43d28c4fa315d4a
Ryan Liu <azhai@126.com>
2021-05-21 20:50:22 +0800

    增加权重和奖池测试

---
+++ application/libraries/Util/Jackpot.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * hack-igniter
+ *
+ * A example project extends of CodeIgniter v3.x
+ *
+ * @package hack-igniter
+ * @author  Ryan Liu (azhai)
+ * @link    http://azhai.surge.sh/
+ * @copyright   Copyright (c) 2013
+ * @license http://opensource.org/licenses/MIT  MIT License
+ */
+
+namespace Mylib\Util;
+
+require_once __DIR__ . '/RoundRobin.php';
+
+/**
+ * 奖池
+ * Author: 阿债 https://azhai.surge.sh
+ */
+class Jackpot
+{
+    protected $id = '';
+    protected $eggs = 0;     // 总抽奖次数
+    protected $dozen = 1;    // 连抽次数
+    protected $prizes = [];  // 奖品权重
+    public $robin;
+    public $redis;
+
+    public function __construct(array $prizes, $eggs, $dozen = 1)
+    {
+        $this->eggs = $eggs;
+        $this->dozen = $dozen;
+        $this->prizes = $prizes;
+        $this->robin = new RoundRobin($this->prizes);
+    }
+    
+    public function initCache($redis)
+    {
+        $this->redis = $redis;
+        $this->id = date('Ymd001');
+        $total = $this->robin->reset();
+        if ($total <= 0) {
+            return;
+        }
+        $this->redis->set('eggs:'. $this->id, $this->eggs);
+        $times = floor($this->eggs / $total);
+        $prizes = [];
+        foreach ($this->prizes as $key => $weight) {
+            $prizes[$key] = $weight * $times;
+        }
+        $prizes[$key] += $this->eggs - $total * $times; // 补给最低奖
+        $this->redis->hmSet('przs:'. $this->id, $prizes);
+    }
+
+    /**
+     * 执行抽奖
+     */
+    public function draw()
+    {
+        $remain = $this->redis->decrBy('eggs:'. $this->id, $this->dozen);
+        if ($remain < 0) {
+            $this->redis->set('eggs:'. $this->id, 0);
+            return [];
+        }
+        $keys = array_keys($this->prizes);
+        $result = array_fill_keys($keys, 0);
+        for ($i = 0; $i < $this->dozen; $i++) {
+            if (($i + $remain) % 2 == -1) {
+                $key = $this->robin->randNext();
+            } else {
+                $key = $this->robin->next();
+            }
+            if ($this->redis->hIncrBy('przs:'. $this->id, $key, -1) >= 0) {
+                $result[$key] += 1;
+            }
+        }
+        ksort($result);
+        return $result;
+    }
+}
+
+
+function test_jackpot()
+{
+    /* 数据初始化，weight: 权重 */
+    $prizes = ['A'=>1, 'B'=>3, 'C'=>10, 'Z'=> 36];
+    $redis = new \Redis();
+    $redis->connect('127.0.0.1', 6379);
+    $jp = new Jackpot($prizes, 1000, 10);
+    $jp->initCache($redis);
+    
+    /* 模拟30次 */
+    $result = [];
+    for ($i = 0; $i < 100; $i++) {
+        $result[] = json_encode($jp->draw());
+    }
+    $result[] = "\n";
+    
+    /* 输出结果 */
+    return implode("\n", $result);
+}
+
+// echo test_jackpot();
+
---
+++ application/libraries/Util/RoundRobin.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * hack-igniter
+ *
+ * A example project extends of CodeIgniter v3.x
+ *
+ * @package hack-igniter
+ * @author  Ryan Liu (azhai)
+ * @link    http://azhai.surge.sh/
+ * @copyright   Copyright (c) 2013
+ * @license http://opensource.org/licenses/MIT  MIT License
+ */
+
+namespace Mylib\Util;
+
+/**
+ * 权重轮询
+ * Author: 阿债 https://azhai.surge.sh
+ */
+class RoundRobin
+{
+    protected $data = [];      // 原始权重键值对
+    protected $currents = [];  // 当前累积权重
+    protected $total = 0;      // 权重总和
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+        $this->reset();
+    }
+    
+    public function reset()
+    {
+        $keys = array_keys($this->data);
+        $this->currents = array_fill_keys($keys, 0); //PHP5.2+
+        $this->total = array_sum($this->data);
+        return $this->total;
+    }
+
+    /**
+     * 执行一次
+     */
+    public function next()
+    {
+        $best = '';
+        $max_weight = 0;
+        foreach ($this->data as $key => $weight) {
+            $current_weight = $this->currents[$key] += $weight;
+            if ($current_weight >= $max_weight) {
+                $best = $key;
+                $max_weight = $current_weight;
+            }
+        }
+        $this->currents[$best] -= $this->total;
+        return $best;
+    }
+
+    /**
+     * 强制选中一次
+     */
+    public function setNext($best)
+    {
+        foreach ($this->data as $key => $weight) {
+            $this->currents[$key] += $weight;
+        }
+        $this->currents[$best] -= $this->total;
+        return $best;
+    }
+
+    /**
+     * 随机选中一次
+     */
+    public function randNext()
+    {
+        $best = '';
+        $idx = mt_rand(1, $this->total);
+        foreach ($this->data as $key => $weight) {
+            if (empty($best) && $idx >= $weight) {
+                $idx -= $weight;
+                $best = $key;
+            }
+            $this->currents[$key] += $weight;
+        }
+        $this->currents[$best] -= $this->total;
+        return $best;
+    }
+}
+
+
+function test_rrd()
+{
+    /* 数据初始化，weight: 权重 */
+    $data = ['A'=>5, 'B'=>2, 'C'=>1];
+    $rrd = new RoundRobin($data);
+    $times = 80;
+    
+    /* 模拟30次 */
+    $result = [];
+    for ($i = 0; $i < $times; $i++) {
+        if ($i > 0 && $i % 10 == 0) {
+            $result[] = '|';
+        }
+        if ($i > 0 && $i % 16 == 0) { //周期
+            $result[] = "\t";
+        }
+        $result[] = $rrd->next();
+    }
+    $result[] = "\n";
+    
+    
+    /* 模拟30次 */
+    $rrd->reset();
+    for ($i = 0; $i < $times; $i++) {
+        if ($i > 0 && $i % 10 == 0) {
+            $result[] = '|';
+        }
+        if ($i > 0 && $i % 16 == 0) { //周期
+            $result[] = "\t";
+        }
+        if ($i == 8) {
+            $result[] = $rrd->setNext('B');
+        } else {
+            $result[] = $rrd->next();
+        }
+    }
+    $result[] = "\n";
+    
+    
+    /* 模拟30次 */
+    $rrd->reset();
+    for ($i = 0; $i < $times; $i++) {
+        if ($i > 0 && $i % 10 == 0) {
+            $result[] = '|';
+        }
+        if ($i > 0 && $i % 16 == 0) { //周期
+            $result[] = "\t";
+        }
+        if ($i == 8) {
+            $result[] = $rrd->setNext('C');
+        } else {
+            $result[] = $rrd->next();
+        }
+    }
+    $result[] = "\n";
+    $result[] = "\n";
+    
+    
+    /* 模拟30次 */
+    $rrd->reset();
+    for ($i = 0; $i < $times; $i++) {
+        if ($i > 0 && $i % 10 == 0) {
+            $result[] = '|';
+        }
+        if ($i > 0 && $i % 16 == 0) { //周期
+            $result[] = "\t";
+        }
+        if ($i % 4 == 0) {
+            $result[] = $rrd->randNext();
+        } else {
+            $result[] = $rrd->next();
+        }
+    }
+    $result[] = "\n";
+    
+    
+    /* 模拟30次 */
+    $rrd->reset();
+    for ($i = 0; $i < $times; $i++) {
+        if ($i > 0 && $i % 10 == 0) {
+            $result[] = '|';
+        }
+        if ($i > 0 && $i % 16 == 0) { //周期
+            $result[] = "\t";
+        }
+        if ($i % 2 == 0) {
+            $result[] = $rrd->randNext();
+        } else {
+            $result[] = $rrd->next();
+        }
+    }
+    $result[] = "\n";
+    
+    
+    /* 模拟30次 */
+    $rrd->reset();
+    for ($i = 0; $i < $times; $i++) {
+        if ($i > 0 && $i % 10 == 0) {
+            $result[] = '|';
+        }
+        if ($i > 0 && $i % 16 == 0) { //周期
+            $result[] = "\t";
+        }
+        if ($i % 4 > 0) {
+            $result[] = $rrd->randNext();
+        } else {
+            $result[] = $rrd->next();
+        }
+    }
+    $result[] = "\n";
+    
+    /* 输出结果 */
+    return implode(' ', $result);
+}
+
+// echo str_replace("\n ", "\n", test_rrd());
+
